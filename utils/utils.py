1a3bb608df9679c2e6a16c3431d4d826c1ad6c40
fcdl94 <S203813s3>
2021-04-01 16:26:42 +0200

    Fix CTS cMap

--- utils/utils.py
+++ utils/utils.py
@@ -35,19 +35,23 @@ def fix_bn(model):
 
 
 def color_map(dataset):
-    if dataset=='voc':
+    if dataset == 'voc':
         return voc_cmap()
-    elif dataset=='cityscapes':
+    elif dataset == 'cts':
         return cityscapes_cmap()
-    elif dataset=='ade':
+    elif dataset == 'ade':
         return ade_cmap()
 
 
-def cityscapes_cmap():
-    return np.array([(128, 64,128), (244, 35,232), ( 70, 70, 70), (102,102,156), (190,153,153), (153,153,153), (250,170, 30), 
-                         (220,220,  0), (107,142, 35), (152,251,152), ( 70,130,180), (220, 20, 60), (255,  0,  0), (  0,  0,142), 
-                         (  0,  0, 70), (  0, 60,100), (  0, 80,100), (  0,  0,230), (119, 11, 32), (  0,  0,  0)], 
-                         dtype=np.uint8)
+def cityscapes_cmap(N=256):
+    color = [(128, 64,128), (244, 35,232), ( 70, 70, 70), (102,102,156), (190,153,153), (153,153,153), (250,170, 30),
+                         (220,220,  0), (107,142, 35), (152,251,152), ( 70,130,180), (220, 20, 60), (255,  0,  0), (  0,  0,142),
+                         (  0,  0, 70), (  0, 60,100), (  0, 80,100), (  0,  0,230), (119, 11, 32), (  0,  0,  0)]
+    cmap = np.zeros((N, 3), dtype='uint8')
+    for i, co in enumerate(color):
+        cmap[i] = co
+
+    return cmap.astype(np.uint8)
 
 
 def ade_cmap():
